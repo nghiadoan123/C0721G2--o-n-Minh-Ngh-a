be7fd2fab1ac2ec046160ab30cd3c6acc066694c
nghiadoan123 <minhnghia191189@gmail.com>
2021-09-04 06:47:32 +0700

    fix_bt_loop_20fisrt_prime

--- modul__2/src/_02_loop_in_java/exercise/Díplay20FirstIsPrime.java
+++ modul__2/src/_02_loop_in_java/exercise/Díplay20FirstIsPrime.java
@@ -4,18 +4,12 @@ import java.util.Scanner;
 
 public class Díplay20FirstIsPrime {
     public static void main(String[] args) {
-        int number;
         int count = 0;
         boolean flag;
         int n = 2;
         String result = "";
-        do {
-            System.out.println("mời nhập vào số lượng số nguyên tố cần hiển thị <=20");
-            Scanner sc = new Scanner(System.in);
-            number = sc.nextInt();
-        } while (number > 20);
-
-        while (count < number) {
+        System.out.print("display 20 first prime:\n");
+        while (count < 20) {
             flag = true;
             if (n < 2) {
                 flag = false;
