1b040e8da27eab33da1951ba5f31827006e75210
nghiadoan123 <minhnghia191189@gmail.com>
2021-09-27 11:41:06 +0700

    up bt 19

--- modul__2/src/_19_string_regex/exercise/validate_phone_number/TestPhoneNumber.java
+++ modul__2/src/_19_string_regex/exercise/validate_phone_number/TestPhoneNumber.java
@@ -1,5 +1,19 @@
 package _19_string_regex.exercise.validate_phone_number;
 
 public class TestPhoneNumber {
+    public static final String[] validPhoneNumber = new String[] { "(84)-(0978489648)", "(80)-(0978434648)"};
+    public static final String[] invalidPhoneNumber = new String[] { "84)-(0978489648)", "(84)-(09784896482)"};
 
+    public static void main(String[] args) {
+        for (String phoneNum: validPhoneNumber) {
+            boolean isValid = ValidatePhoneNumber.validatePhoneNumber(phoneNum);
+            System.out.println("phone number  is " + phoneNum +" is valid: "+ isValid);
+        }
+
+        for (String phoneNum: invalidPhoneNumber) {
+            boolean isValid = ValidatePhoneNumber.validatePhoneNumber(phoneNum);
+            System.out.println("phone number is " + phoneNum +" is valid: "+ isValid);
+        }
+
+    }
 }
--- modul__2/src/_19_string_regex/exercise/validate_phone_number/ValidatePhoneNumber.java
+++ modul__2/src/_19_string_regex/exercise/validate_phone_number/ValidatePhoneNumber.java
@@ -2,7 +2,7 @@ package _19_string_regex.exercise.validate_phone_number;
 
 public class ValidatePhoneNumber {
     public static boolean validatePhoneNumber(String name){
-        String regex = "[(]\\d{2}[)]-";
+        String regex = "^[(]\\d{2}[)]-[(][0]\\d{9}[)]$";
         return name.matches(regex);
     }
 
